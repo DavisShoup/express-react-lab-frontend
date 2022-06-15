69200a203602321ee149f37c05df972321f285e5
Davis Shoup <davisshoup@yahoo.com>
2022-06-14 20:44:40 -0400

    frontend complete.

---
+++ src/pages/About.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react'
+
+const About = (props) => {
+    const [ about, setAbout ] = useState(null);
+    const getAboutData = async () => {
+        const response = await fetch(props.URL + "about");
+        const data = await response.json();
+        setAbout(data);
+    }
+
+    useEffect(() => {
+        getAboutData();
+    });
+
+    const loaded = () => {
+        return(
+        <div>
+            <h2>{about.name}</h2>
+            <h3>{about.email}</h3>
+            <p>{about.bio}</p>
+        </div>
+    )};
+
+    const loading = () => {
+        return <h1>Loading...</h1>
+}
+
+
+    return about ? loaded() : loading();
+
+}
+
+export default About;
---
+++ src/pages/Projects.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+
+const Projects = (props) => {
+    const [ projects, setProjects ] = useState(null);
+    const getProjectsData = async () => {
+        const response = await fetch(props.URL + "projects");
+        const data = await response.json();
+        setProjects(data);
+    };
+    useEffect(() => {
+    getProjectsData();
+    }, []);
+
+    const loaded = () => {
+        console.log(projects)
+        return projects.map(project => (
+            <div>
+                <h1>{project.name}</h1>
+                <img className="project-image" src={project.image} alt={project.name}/>
+                <a href={project.git}>
+                    <button>Github</button>
+                </a>
+                <a href={project.live}>
+                    <button>Live site</button>
+                </a>
+            </div>
+        ));
+    };
+
+    const loading = () => {
+        return <h1>Loading ...</h1>
+    }
+
+    return projects ? loaded() : loading();
+
+}
+
+export default Projects;
